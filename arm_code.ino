734de49b90cbe8550e624db2ae2c0aaa7e096c5e
sanjayu06 <sumasha@terpmail.umd.edu>
2024-11-18 19:43:21 -0500

    PLSSSSSSSSSSS LET ME INNNNNNNNNNN
    PLSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- arm_code.ino
+++ arm_code.ino
@@ -1,5 +1,21 @@
 const int EN_A=3;
-const int IN_1A=22;
-const int IN_2A=23;
+const int IN1_A=22;
+const int IN2_A=23;
 
 
+void setup() {
+  pinMode(EN_A, OUTPUT);
+  pinMode(IN1_A, OUTPUT);
+  pinMode(IN2_A, OUTPUT);
+}
+
+void loop() {
+  analogWrite(EN_A, 25);
+  digitalWrite(IN1_A, LOW);
+  digitalWrite(IN2_A, HIGH);
+  delay(1500);
+
+  digitalWrite(IN1_A, LOW);
+  digitalWrite(IN2_A, LOW);
+  delay(1000000000000);
+}
